252bdd23d67a9bc281b850a5a67d1f406ca828de
Fency Git <1397970059@qq.com>
2025-09-19 22:32:28 +0800

    后端 - 初始化
    1. 整合常用依赖
    2. 编写各种配置

---
+++ coder-practice-backend/src/main/java/com/codebear/coderpracticebackend/CoderPracticeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codebear.coderpracticebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderPracticeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoderPracticeBackendApplication.class, args);
+    }
+
+}
---
+++ coder-practice-backend/src/test/java/com/codebear/coderpracticebackend/CoderPracticeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codebear.coderpracticebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoderPracticeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
